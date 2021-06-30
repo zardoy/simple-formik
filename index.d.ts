fa9b1752740236ee4ff3200ecd652defa8c24a4e
zardoy <zardoy@users.noreply.github.com>
2021-06-30 16:37:03 +0000

    deploy: 150f3e092da5979eeb8756b8d1ae1e7f000d76a0

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import React from "react";
+declare type UV = Record<string, string>;
+declare type HandleOptions = {};
+declare type Options<T extends UV> = {
+    initialValues: T;
+    onSubmit: (values: T) => void;
+    /** All fields are required, if some of them isn't filled, `disableSubmitButton` will be `false` */
+    allRequired: true;
+};
+declare type Return<T extends UV> = Readonly<{
+    handleSubmit: Exclude<React.ComponentProps<"form">["onSubmit"], undefined>;
+    handleInput: Record<keyof T, (options: HandleOptions) => Pick<React.ComponentProps<"input">, "value" | "onChange">>;
+    values: T;
+    setValue: <K extends keyof T>(name: K, value: T[K]) => void;
+    disableSubmitButton: boolean;
+}>;
+export declare const useSimpleFormik: <T extends UV>({ initialValues, onSubmit }: Options<T>) => Readonly<{
+    handleSubmit: Exclude<React.ComponentProps<"form">["onSubmit"], undefined>;
+    handleInput: Record<keyof T, (options: HandleOptions) => Pick<React.ComponentProps<"input">, "value" | "onChange">>;
+    values: T;
+    setValue: <K extends keyof T>(name: K, value: T[K]) => void;
+    disableSubmitButton: boolean;
+}>;
+export {};
